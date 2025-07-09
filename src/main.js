7fb8256c8fa84ce3e4d1d1da578dbcd98dba4b4c
Lew257 <gerhardxmc@gmail.com>
2025-07-09 13:24:07 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
